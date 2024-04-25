f7661d558b74ce65b05a629f2b761962982e6293
ylw <1756073912@qq.com>
2024-04-25 14:36:53 +0800

    五维数据

---
+++ src/main/java/com/example/wuweishuju/WuweishujuApplication.java
@@ -0,0 +1,13 @@
+package com.example.wuweishuju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuweishujuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WuweishujuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wuweishuju/WuweishujuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wuweishuju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WuweishujuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
